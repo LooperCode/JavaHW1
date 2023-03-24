fe7bde154f6d182af59eead25ca87ade54bd1449
LooperCode <savchenkodkz@gmail.com>
2023-03-24 06:10:53 +0600

    upd comment

--- Ex3.java
+++ Ex3.java
@@ -2,7 +2,7 @@ package JavaHW1;
 
 import java.util.Scanner;
 /* Написать пройстейший калькулятор. */
-/*UPD:// Добавить в него функцию отмены действия */
+/*UPD:// Добавить в него функцию отмены последней операции */
 public class Ex3 {
     static Scanner scan = new Scanner(System.in);
 
